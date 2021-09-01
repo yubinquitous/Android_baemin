fbb5b32c84d994b24513dddb9b46b90a1d4b9145
SSU\yb9854 <yb9854@naver.com>
2021-09-01 14:36:17 +0900

    배달의 민족 홈화면

---
+++ app/src/main/java/com/example/st_w1_baemin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.st_w1_baemin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
